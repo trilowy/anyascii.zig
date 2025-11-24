41d4f99ecda406264522dbed92190ce1263f2ce9
Trilowy <49493635+trilowy@users.noreply.github.com>
2025-11-24 19:43:22 +0100

    Format code with zig fmt.

--- build.zig
+++ build.zig
@@ -1,29 +1,29 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-	const target = b.standardTargetOptions(.{});
-	const optimize = b.standardOptimizeOption(.{});
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
 
-	// Anyascii zig module.
-	const anyascii = b.addModule("anyascii", .{
-		.root_source_file = b.path("src/lib.zig"),
-		.target = target,
-		.optimize = optimize,
-	});
-	anyascii.link_libc = true;
-	anyascii.addIncludePath(b.path("anyascii"));
-	anyascii.addCSourceFile(.{
-		.file = b.path("anyascii/anyascii.c"),
-	});
+    // Anyascii zig module.
+    const anyascii = b.addModule("anyascii", .{
+        .root_source_file = b.path("src/lib.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    anyascii.link_libc = true;
+    anyascii.addIncludePath(b.path("anyascii"));
+    anyascii.addCSourceFile(.{
+        .file = b.path("anyascii/anyascii.c"),
+    });
 
-	// Library unit tests.
-	const lib_unit_tests = b.addTest(.{
-		.root_module = anyascii,
-		.target = target,
-		.optimize = optimize,
-	});
-	const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+    // Library unit tests.
+    const lib_unit_tests = b.addTest(.{
+        .root_module = anyascii,
+        .target = target,
+        .optimize = optimize,
+    });
+    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
-	const test_step = b.step("test", "Run unit tests");
-	test_step.dependOn(&run_lib_unit_tests.step);
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_lib_unit_tests.step);
 }
